73115bd07663498f3b665025e8c8027f0c4f16af
Arnav Jain <arnjain3@gmail.com>
2023-11-16 00:38:15 +0530

    added function comment for getAlphabetMatrix

--- alphabetMatrix.py
+++ alphabetMatrix.py
@@ -2,6 +2,9 @@ import json
 
 
 def getAlphabetMatrix() -> dict:
+    """
+    Returns jsonified `alphabetMatrix.json`
+    """
     with open("alphabetMatrix.json", "r") as f:
         JSON = f.read()
         return json.loads(JSON)
