5ef692b702e0f7d89c2b58bc5843e497038288d0
Arnav Jain <arnjain3@gmail.com>
2023-11-16 02:48:50 +0530

    maybe will work
    added set -e and did dir stuff

--- commitOrNot.sh
+++ commitOrNot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -e
+
 # Check if datesToCommit.json exists
 if [ ! -f datesToCommit.json ]; then
     echo "Error: datesToCommit.json not found. Run main.py to generate it."
     exit 1
 fi
 
+gicogDir=~
 todayDate=$(date +"%Y-%m-%d")
 
 # Check if the file exists
@@ -17,10 +20,10 @@ if [[ -f "datesToCommit.json" ]]; then
     if [[ $datesToCommit -eq 1 ]]; then
         # Save a random string to commit.txt
         randomString=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 32)
-        echo "$randomString" > commit.txt
+        echo "$randomString" > $gicogDir/commit.txt
     else
         # If commit is not 1, clear commit.txt
-        "" > commit.txt
+        "" > $gicogDir/commit.txt
     fi
 else
     echo "Error: Did not find $todayDate in datesToCommit.json"
@@ -28,12 +31,12 @@ else
 fi
 
 # Check if commit.txt has contents
-str=$(cat commit.txt)
+str=$(cat $gicogDir/commit.txt)
 strLen=${#str}
 
 if [ $strLen -gt 0 ]; then
     # Add, commit, and push changes
-    git add commit.txt
+    git add $gicogDir/commit.txt
     git commit -m "$(date '+%Y-%m-%d')"
     git push
 fi
