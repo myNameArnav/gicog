f13ef51665f1a45dd1355f7228a6175fa1323795
Arnav Jain <35961071+myNameArnav@users.noreply.github.com>
2023-11-20 22:43:09 +0530

    Update commitOrNot.sh

--- commitOrNot.sh
+++ commitOrNot.sh
@@ -8,7 +8,6 @@ if [ ! -f datesToCommit.json ]; then
     exit 1
 fi
 
-gicogDir=~
 todayDate=$(date +"%Y-%m-%d")
 
 # Check if the file exists
@@ -16,14 +15,16 @@ if [[ -f "datesToCommit.json" ]]; then
     # Extract the value for today's date from datesToCommit.json
     datesToCommit=$(jq -r ".\"$todayDate\"" datesToCommit.json)
 
+    echo "Script ran at $todayDate, and isCommit is $datesToCommit" >> log.txt
+
     # Check if commit = 1
     if [[ $datesToCommit -eq 1 ]]; then
         # Save a random string to commit.txt
         randomString=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 32)
-        echo "$randomString" > $gicogDir/commit.txt
+        echo "$randomString" > commit.txt
     else
         # If commit is not 1, clear commit.txt
-        > $gicogDir/commit.txt
+        > commit.txt
     fi
 else
     echo "Error: Did not find $todayDate in datesToCommit.json"
@@ -31,12 +32,11 @@ else
 fi
 
 # Check if commit.txt has contents
-str=$(cat $gicogDir/commit.txt)
+str=$(cat commit.txt)
 strLen=${#str}
 
 if [ $strLen -gt 0 ]; then
     # Add, commit, and push changes
-    git add $gicogDir/commit.txt
-    git commit -m "$(date '+%Y-%m-%d')"
-    git push
+    git commit -am "$(date '+%Y-%m-%d')"
+    git push -u origin main
 fi
